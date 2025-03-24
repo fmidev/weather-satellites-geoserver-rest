64d89341945cdd32514f6d56fb143e8428901f13
Panu Lahtinen <panu.lahtinen@fmi.fi>
2025-03-24 15:02:50 +0200

    Add SIGTERM handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
 
+_term() {
+  echo "Entrypoint caught SIGTERM signal"
+  kill -TERM "$child" 2>/dev/null
+  echo "Waiting for child process to exit"
+  wait "$child"
+}
+
+trap _term SIGTERM
+
 source /opt/conda/.bashrc
 source /config/env-variables
 
@@ -9,7 +18,8 @@ micromamba activate
 CLEANING_CYCLE=21600
 
 if [ -e /config/geoserver_posttroll_update.yaml ]; then
-    /opt/conda/bin/posttroll_adder.py /config/geoserver_posttroll_update.yaml
+    /opt/conda/bin/posttroll_adder.py /config/geoserver_posttroll_update.yaml &
+    child=$!
 elif [ -e /config/geoserver_add_granule.yaml ]; then
     /opt/conda/bin/add_granule.py /config/geoserver_add_granule.yaml
 elif [ -e /config/geoserver_delete_old_granules.yaml ]; then
@@ -18,3 +28,5 @@ elif [ -e /config/geoserver_delete_old_granules.yaml ]; then
 	sleep $CLEANING_CYCLE
     done
 fi
+
+wait "$child"
